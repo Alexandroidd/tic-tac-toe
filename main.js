39580bbd0a8939d492faa584ab185ff5ad31336f
alexandroidd <alexandroidcreighton@gmail.com>
2017-03-22 22:36:26 -0600

    adds X's and O's plus class changes each click + player profile

--- main.js
+++ main.js
@@ -1,22 +1,69 @@
 console.log("this is working");
 
+// PLAYER DATA ----
+var playerOne = {
+	name: "I am making an X",
+	mark: "X"
+};
+var playerTwo = {
+	name: "I am making an O",
+	mark: "O"
+};
+
+var currentPlayer;
+var players = [playerOne, playerTwo];
+
+// -------------
 
 
 
 
-// var one = $('td.one');
-// var two = $('td.two');
-// var three = $('td.three');
-// var four = $('td.four');
-// var five = $('td.five');
-// var six = $('td.six');
-// var seven = $('td.seven');
-// var eight = $('td.eight');
-// var nine = $('td.nine');
+// DETERMINES WHO STARTS --------
+$(document).ready(function() {
+var startNum = Math.floor((Math.random() * 2) + 1);
+	if (startNum === 1) {
+		currentPlayer = playerOne;
+	} else if (startNum === 2) {
+		currentPlayer = playerTwo;
+	}
+console.log(currentPlayer);
+});
+
+
+
+
 
 // ---This applies symbols to boxes when clicked----
-$(".playSquares td").on("click", function(event){
-    $(this).text("x");
+
+$(".playSquares td").on("click", function(event) {
+		if ($(this).attr('class','notPlayed') && currentPlayer === playerOne) {
+			$(this).text(currentPlayer.mark);
+			$(this).attr('class', 'X');
+			currentPlayer = playerTwo;
+			console.log($(this));
+		} else if ($(this).attr('class','notPlayed') && currentPlayer === playerTwo) {
+			$(this).text(currentPlayer.mark);
+			$(this).attr('class', 'O');
+			currentPlayer = playerOne;
+			console.log($(this));
+		}
+
+
+
+		// {
+		// 	if (currentPlayer === playerOne) {
+		// 		$(this).text("X");
+		// 		currentPlayer = playerTwo;
+		// 		$(this).class = "played";
+		// 	} else if (currentPlayer === playerTwo) {
+		// 		$(this).text('O');
+		// 		currentPlayer = playerOne;
+		// 		$(this).class = "played";
+		// 	}
+		// } else if (this.text === "O" || "X") {
+		// 	return;
+		// }
+
 });
 
 
