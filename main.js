d5ff76ac4afcdc65f6829811e1087c59f2a904e2
alexandroidd <alexandroidcreighton@gmail.com>
2017-03-23 21:23:45 -0600

    Alert for winner added/fixed

--- main.js
+++ main.js
@@ -29,6 +29,7 @@ var startNum = Math.floor((Math.random() * 2) + 1);
 		currentPlayer = playerTwo;
 	}
 console.log(currentPlayer);
+
 });
 // --------------------------------
 
@@ -40,28 +41,59 @@ console.log(currentPlayer);
 $(".playSquares td").on("click", function(event) {
 		if ($(this).attr('class','notPlayed') && currentPlayer === playerOne) {
 			$(this).text(currentPlayer.mark);
-			board.push( $(this).attr('id'));
 			$(this).attr('class', 'X played');
+			
 			currentPlayer = playerTwo;
 			console.log($(this));
 		} else if ($(this).attr('class','notPlayed') && currentPlayer === playerTwo) {
 			$(this).text(currentPlayer.mark);
-			board.push(currentPlayer.mark);
+			
 			$(this).attr('class', 'O played');
 			currentPlayer = playerOne;
 			console.log($(this));
 		}
-console.log(board);
+		checkWin();
+
 });
+
+
 // ----------------------------------
 
 // SCORING ----
 
-var board = [];
+var checkWin = function() {
+if ($('#one').hasClass('X') && $('#two').hasClass('X') && $('#three').hasClass('X') || 
+	$('#one').hasClass('X') && $('#four').hasClass('X') && $('#seven').hasClass('X') ||
+	$('#one').hasClass('X') && $('#five').hasClass('X') && $('#nine').hasClass('X') || 
+	$('#two').hasClass('X') && $('#five').hasClass('X') && $('#eight').hasClass('X') || 
+	$('#three').hasClass('X') && $('#five').hasClass('X') && $('#seven').hasClass('X') ||
+	$('#three').hasClass('X') && $('#six').hasClass('X') && $('#nine').hasClass('X') || 
+	$('four').hasClass('X') && $('#five').hasClass('X') && $('#six').hasClass('X') ||
+	$('#seven').hasClass('X') && $('#eight').hasClass('X') && $('#nine').hasClass('X')) {
+		
+		alert("Player One has won!!");
+
+}  else if ($('#one').hasClass('O') && $('#two').hasClass('O') && $('#three').hasClass('O') ||
+	$('#one').hasClass('O') && $('#four').hasClass('O') && $('#seven').hasClass('O') ||
+	$('#one').hasClass('O') && $('#five').hasClass('O') && $('#nine').hasClass('O') || 
+	$('#two').hasClass('O') && $('#five').hasClass('O') && $('#eight').hasClass('O') || 
+	$('#three').hasClass('O') && $('#five').hasClass('O') && $('#seven').hasClass('O') ||
+	$('#three').hasClass('O') && $('#six').hasClass('O') && $('#nine').hasClass('O') || 
+	$('four').hasClass('O') && $('#five').hasClass('O') && $('#six').hasClass('O') ||
+	$('#seven').hasClass('O') && $('#eight').hasClass('O') && $('#nine').hasClass('O')) {
+		
+		alert("Player Two has won!!");
+}
+};
+
+
+		// if (playerOneBoard[0] === "one") {
+		// 	console.log("PLAYER ONE WINS");
+		// }
 
 
 
-
+//add counter var
 
 
 
@@ -72,8 +104,10 @@ var board = [];
 $("#reset").on("click", function(event) {
 	$('.playSquares td').empty();
 	$('.playSquares td').text('');
+	$('.playSquares td').attr('class',"");
 	$('.playSquares td').attr('class','notPlayed');
-	board = [];
+	
+	
 });
 // -----------------------
 
